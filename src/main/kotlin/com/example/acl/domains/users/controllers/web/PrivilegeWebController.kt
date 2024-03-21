2843dd937b46d830565325158a641dfbda2fcd24
Sayem Hossain <mail@sayem.dev>
2024-03-22 00:46:55 +0600

    Fixed privilege update page

--- src/main/kotlin/com/example/acl/domains/users/controllers/web/PrivilegeWebController.kt
+++ src/main/kotlin/com/example/acl/domains/users/controllers/web/PrivilegeWebController.kt
@@ -1,10 +1,13 @@
 package com.example.acl.domains.users.controllers.web
 
+import arrow.core.getOrElse
+import arrow.core.toOption
 import com.example.common.utils.ExceptionUtil
 import com.example.acl.domains.users.models.dtos.PrivilegeDto
 import com.example.acl.domains.users.models.mappers.PrivilegeMapper
 import com.example.acl.domains.users.services.PrivilegeService
 import com.example.acl.routing.Route
+import com.example.common.exceptions.toArrow
 import com.example.coreweb.listeners.EndpointsListener
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.HttpMethod
@@ -15,8 +18,8 @@ import javax.validation.Valid
 
 @Controller
 class PrivilegeWebController @Autowired constructor(
-        private val privilegeService: PrivilegeService,
-        private val privilegeMapper: PrivilegeMapper
+    private val privilegeService: PrivilegeService,
+    private val privilegeMapper: PrivilegeMapper
 ) {
 
     @GetMapping(Route.V1.WEB_PRIVILEGES_PAGE)
@@ -43,11 +46,16 @@ class PrivilegeWebController @Autowired constructor(
     }
 
     @GetMapping(Route.V1.WEB_PRIVILEGE_DETAILS_PAGE)
-    fun privilegeDetailsPage(@PathVariable("privilege_id") privilegeId: Long,
-                             model: Model): String {
+    fun privilegeDetailsPage(
+        @PathVariable("privilege_id") privilegeId: Long,
+        model: Model
+    ): String {
 
-        val selectedPrivilege = this.privilegeService.find(privilegeId).orElseThrow { ExceptionUtil.notFound("Privilege", privilegeId) }
+        val selectedPrivilege = this.privilegeService.find(privilegeId).toArrow()
+            .map { it.apply { this.urlAccesses = privilegeService.findAccesses(it.id) } }
+            .getOrElse { throw ExceptionUtil.notFound("Privilege", privilegeId) }
         val privileges = this.privilegeService.findAll()
+            .map { it.apply { this.urlAccesses = privilegeService.findAccesses(it.id) } }
 
         model.addAttribute("selectedPrivilege", selectedPrivilege)
         model.addAttribute("privileges", privileges)
@@ -63,9 +71,12 @@ class PrivilegeWebController @Autowired constructor(
     }
 
     @PostMapping(Route.V1.WEB_PRIVILEGE_UPDATE)
-    fun updatePrivilege(@PathVariable("privilege_id") privilegeId: Long,
-                        @Valid @ModelAttribute privilegeDto: PrivilegeDto): String {
-        var privilege = this.privilegeService.find(privilegeId).orElseThrow { ExceptionUtil.notFound("Privilege", privilegeId) }
+    fun updatePrivilege(
+        @PathVariable("privilege_id") privilegeId: Long,
+        @Valid @ModelAttribute privilegeDto: PrivilegeDto
+    ): String {
+        var privilege =
+            this.privilegeService.find(privilegeId).orElseThrow { ExceptionUtil.notFound("Privilege", privilegeId) }
         if (privilege.name == "ADMINISTRATION") throw ExceptionUtil.forbidden("Updating privilege ADMINISTRATION is not possible.")
         privilege = this.privilegeService.save(this.privilegeMapper.map(privilegeDto, privilege))
         return "redirect:/admin/privileges/${privilege.id}"
